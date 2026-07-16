01df3e3e059bb212a4181a9246b646febc72145a
Panos Synodinos <geroulakou.k@gmail.com>
2026-07-16 12:41:02 +0300

    create components

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function searchBar() {
+
+    return (
+    <div>
+        <input type="text" name="searchBar" />;
+        <button name="search">Search</button>
+    </div>
+    );
+}
+
+export default SearchBar;
